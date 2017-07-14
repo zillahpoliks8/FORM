7cc5175ad5676d3c7fb0b1fd7eaab687554dedc8
unknown <poliquitzillahit1b@gmail.com>
2017-07-14 11:33:27 +0800

    1st commitu

---
+++ server.js
@@ -0,0 +1,17 @@
+
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port 5000',app.get('port'));
+});
